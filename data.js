6f4dd196896b380cb5f14460b7e18225a2fb719a
nglthu <nglthu@gmail.com>
2016-10-27 13:57:45 +1300

    Updated

---
+++ data.js
@@ -0,0 +1,52 @@
+var viewModel = {
+			locations: [{
+				title: 'Subway'
+				, location: {
+					lat: -36.8737632
+					, lng: 174.7936569
+				}
+				, detail: "Eat Fresh"
+			}, {
+				title: 'Auckland Town Hall'
+				, location: {
+					lat: -36.8530296
+					, lng: 174.7630188
+				}
+				, detail: 'A historic building on Queen Street'
+			}, {
+				title: 'AUT'
+				, location: {
+					lat: -36.9196601
+					, lng: 174.7545022
+				}
+				, detail: 'The University for The Changing World'
+			}, {
+				title: 'Central City Library'
+				, location: {
+					lat: -36.851759
+					, lng: 174.7632119
+				}
+				, detail: 'Public Library'
+			}, {
+				title: 'Q Theatre'
+				, location: {
+					lat: -36.8533432
+					, lng: 174.7610524
+				}
+				, detail: 'Auckland newest space for the performing arts'
+			}, {
+				title: 'The Civic'
+				, location: {
+					lat: -36.851036
+					, lng: 174.7617099
+				}
+				, detail: 'First opened on 20 December 1929'
+			}, {
+				title: 'Auckland Zoo'
+				, location: {
+					lat: -36.864113
+					, lng: 174.7174963
+				}
+				, detail: '16.35-hectare zoological garden in Auckland'
+			}]
+		};
